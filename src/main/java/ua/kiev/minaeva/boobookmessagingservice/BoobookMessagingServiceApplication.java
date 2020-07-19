5757f92673aaa495f47e764aab64ff7d3878f142
minaeva <sminaeva@uscold.com>
2020-07-19 12:52:32 +0300

    init commit

---
+++ src/main/java/ua/kiev/minaeva/boobookmessagingservice/BoobookMessagingServiceApplication.java
@@ -0,0 +1,13 @@
+package ua.kiev.minaeva.boobookmessagingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoobookMessagingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoobookMessagingServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ua/kiev/minaeva/boobookmessagingservice/BoobookMessagingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.kiev.minaeva.boobookmessagingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoobookMessagingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
