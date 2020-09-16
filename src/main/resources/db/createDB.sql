87e2b05a1cc67a696c3e14283d13396883d94bc9
minaeva <sminaeva@uscold.com>
2020-09-17 00:19:55 +0300

    heroku

--- src/main/resources/db/createDB.sql
+++ src/main/resources/db/createDB.sql
@@ -6,10 +6,18 @@ drop table if exists message;
 
 create table message
 (
-    id         serial  PRIMARY KEY,
-    from_id       bigint       not null,
-    to_id         bigint       not null,
+    id         serial PRIMARY KEY,
+    from_id    bigint       not null,
+    to_id      bigint       not null,
     date_added timestamp,
     message    varchar(500) not null
 );
 
+
+INSERT INTO message(from_id, to_id, date_added, message)
+VALUES (1, 2, current_timestamp, 'message 1'),
+       (2, 1, current_timestamp, 'message 2'),
+       (1, 2, current_timestamp, 'message 3'),
+       (1, 3, current_timestamp, 'message 4'),
+       (4, 2, current_timestamp, 'message 5'),
+       (4, 1, current_timestamp, 'message 6')
